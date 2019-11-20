1168eb57d2d628944dd51eb3952d108e32dd41fe
dokugo <dokugo@users.noreply.github.com>
2019-11-20 03:28:04 +0300

    test json sample serving

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+console.clear();
+
+const express = require('express');
+const cors = require('cors');
+
+const sampleData = require('./sample.json');
+
+const PORT = process.env.PORT || 9000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.listen(PORT, () => console.log(`rymtracker app listening on port ${PORT}`));
+
+
+app.get('/test/', async (req, res) => {
+  try {
+    res.status(200).send(sampleData);
+  } catch (error) {
+    console.log(error);
+  }
+});
