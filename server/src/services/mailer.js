fa4fe8b152b672bcf83944f2832f1f02dfc42cff
dokugo <dokugo@users.noreply.github.com>
2019-12-06 08:17:42 +0300

    email template render logic

--- server/src/services/mailer.js
+++ server/src/services/mailer.js
@@ -1,38 +1,38 @@
 const nodemailer = require('nodemailer');
 const ejs = require('ejs');
 const path = require('path');
-const template = '../templates/email.ejs';
 
-// require('dotenv').config();
-// const pass = process.env.SMTP_API_KEY;
-// const host = process.env.SMTP_HOST;
+require('dotenv').config();
+const host = process.env.SMTP_HOST__PROD || process.env.SMTP_HOST;
+const user = process.env.SMTP_USER__PROD || process.env.SMTP_USER;
+const pass = process.env.SMTP_PASS__PROD || process.env.SMTP_PASS;
 
 const transport = {
-  // host: host,
-  host: 'smtp.mailtrap.io',
+  host: host,
   port: 587, // true for 465, false for other ports
   auth: {
-    user: '1b344bf94c24a8',
-    pass: '0c67fc31f9ab48'
-  }
-  /*       auth: {
-    user: 'apikey',
+    user: user,
     pass: pass
-  } */
+  }
 };
 const transporter = nodemailer.createTransport(transport);
 
-const getHtml = async user => {
+const getHtml = async (user, releases) => {
   const templateData = {
-    name: user.username
+    name: user.username,
+    releases: releases
   };
+
   const templateDir = path.join(__dirname, '../templates/email.ejs');
-  const html = ejs.renderFile(templateDir, templateData);
+
+  const html = await ejs.renderFile(templateDir, templateData);
+
   return html;
 };
 
-const getOptions = async user => {
-  const html = await getHtml(user);
+const getOptions = async (user, releases) => {
+  const html = await getHtml(user, releases);
+
   const options = {
     from: '"RYM Tracker" <mailer@rymtracker.ml>',
     to: user.email,
@@ -40,12 +40,13 @@ const getOptions = async user => {
     text: `Please use a client with HTML support.`,
     html: html
   };
+
   return options;
 };
 
-const mailer = async (user, release) => {
+const mailer = async (user, releases) => {
   try {
-    const options = await getOptions(user);
+    const options = await getOptions(user, releases);
 
     const message = await transporter.sendMail(options);
 
