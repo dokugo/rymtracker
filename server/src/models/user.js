70395a62d118578ede06dd1bffdb157214114cb2
dokugo <dokugo@users.noreply.github.com>
2020-01-06 23:34:35 +0300

    get rid of release model, merge it into user model

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: String,
   email: String,
-  isVerified: Boolean
+  isVerified: Boolean,
+  data: {
+    releases: Array,
+    error: String
+  }
 });
 
 userSchema.set('toJSON', {
