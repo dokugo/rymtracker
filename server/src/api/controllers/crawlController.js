220b0e087a567ec3da2b2ea20a6f6946826d4c7c
dokugo <dokugo@users.noreply.github.com>
2020-01-16 06:13:20 +0300

    add mutex locks to crawling controller

--- server/src/api/controllers/crawlController.js
+++ server/src/api/controllers/crawlController.js
@@ -3,6 +3,8 @@ const sampleData = require('../../temp/sample.json');
 const { sleep } = require('../../helpers/utils');
 const User = require('../../models/user');
 const dataProcessor = require('../../helpers/dataProcessor');
+const Lock = require('../../helpers/mutex');
+const lock = new Lock();
 
 const saveCrawledData = async (data, username, error) => {
   const query = { username: username };
@@ -20,6 +22,13 @@ const saveCrawledData = async (data, username, error) => {
 // get all subscribed users crawled data
 exports.everyone = async (request, response) => {
   try {
+    const LOCK_ID = 0;
+    const isLocked = lock.acquire(LOCK_ID);
+    if (isLocked)
+      return response
+        .status(409)
+        .send({ message: `Your request is still processed.` });
+
     const messagesArray = [];
 
     const users = await User.find();
@@ -34,8 +43,8 @@ exports.everyone = async (request, response) => {
         continue;
       }
 
-      await sleep(1000);
       const rawData = await crawler(username);
+      await sleep(1000);
 
       if (rawData.error) {
         await saveCrawledData(null, username, rawData.error);
@@ -55,6 +64,7 @@ exports.everyone = async (request, response) => {
       console.log(message);
     }
 
+    lock.release(LOCK_ID);
     response.status(200).send({ message: messagesArray });
   } catch (error) {
     console.log(error);
@@ -65,22 +75,39 @@ exports.everyone = async (request, response) => {
 // get specified user crawled data
 exports.specified = async (request, response) => {
   try {
+    const lockID = request.ip;
+
+    const isLocked = lock.acquire(lockID);
+    if (isLocked)
+      return response
+        .status(409)
+        .send({ message: `Your request is still processed.` });
+
     const username = request.params.username;
 
     if (username === 'test') {
       const data = sampleData;
+
       await sleep(1000);
+
+      lock.release(lockID);
       return response.status(200).send({ message: { data } });
     }
 
     const rawData = await crawler(username);
 
     if (rawData.error) {
+      await sleep(1000);
+
+      lock.release(lockID);
       return response.status(400).send({ message: { error: rawData.error } });
     }
 
     const data = dataProcessor(rawData);
 
+    await sleep(1000);
+
+    lock.release(lockID);
     response.status(200).send({ message: { data } });
   } catch (error) {
     console.log(error);
