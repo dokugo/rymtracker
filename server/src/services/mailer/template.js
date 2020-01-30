4c5f6834cc532a65ca17544e3579de19bdb614aa
dokugo <7773972+dokugo@users.noreply.github.com>
2020-01-30 23:46:00 +0300

    use object props instead of switch in template.js

--- server/src/services/mailer/template.js
+++ server/src/services/mailer/template.js
@@ -2,82 +2,75 @@ const ejs = require('ejs');
 const path = require('path');
 
 const template = {
-  getSubject(type, user) {
-    let subject;
-    switch (type) {
-      case 'releases':
-        subject = `💿 Upcoming releases for ${user.username}`;
-        break;
-      case 'verification':
-        subject = `❓ Please confirm subscription to ${user.username}`;
-        break;
-      case 'update':
-        subject = `❓ Please confirm subscription update to ${user.username}`;
-        break;
-      case 'greeting':
-        subject = `✔️ Verification successful`;
-        break;
-    }
-    return subject;
+  getSubject(user, type) {
+    this.subjects = {
+      releases: `💿 Upcoming releases for ${user.username}`,
+      verify: `❓ Please confirm subscription to ${user.username}`,
+      update: `❓ Please confirm subscription update to ${user.username}`,
+      greeting: `✔️ Verification successful`
+    };
+    return this.subjects[type];
   },
 
   async getHtml(user, type) {
-    let templateDir, templateData;
-    switch (type) {
-      case 'releases':
-        templateData = {
+    this.options = {
+      releases: {
+        templateDir: path.join(__dirname, '../../templates/releases.ejs'),
+        templateData: {
           user: user,
           data: user.data.releases
-        };
-        templateDir = path.join(__dirname, '../../templates/releases.ejs');
-        break;
+        }
+      },
 
-      case 'verification':
-        templateData = {
+      verify: {
+        templateDir: path.join(__dirname, '../../templates/verify.ejs'),
+        templateData: {
           user: user,
           link: `https://rymtracker.ml/users/verify/${user.id}`,
           text: ``
-        };
-        templateDir = path.join(__dirname, '../../templates/verification.ejs');
-        break;
+        }
+      },
 
-      case 'update':
-        templateData = {
+      update: {
+        templateDir: path.join(__dirname, '../../templates/verification.ejs'),
+        templateData: {
           user: user,
           link: `https://rymtracker.ml/users/update/${user.id}/${user.username}`,
           text: `update`
-        };
-        templateDir = path.join(__dirname, '../../templates/verification.ejs');
-        break;
+        }
+      },
 
-      case 'greeting':
-        templateData = {
+      greeting: {
+        templateDir: path.join(__dirname, '../../templates/greeting.ejs'),
+        templateData: {
           user: user,
           link: `https://rymtracker.ml/user/unsubscribe/${user.id}`,
           text: `Hi, ${user.email}. The new releases list is being mailed to the subscribers on every Sunday, 18:00 GMT/UTC +0.`
-        };
-        templateDir = path.join(__dirname, '../../templates/greeting.ejs');
-        break;
-    }
+        }
+      }
+    };
 
-    const html = await ejs.renderFile(templateDir, templateData);
+    const dir = this.options[type].templateDir;
+    const data = this.options[type].templateData;
+    const html = await ejs.renderFile(dir, data);
     return html;
   },
 
   getOptions(user, subject, html) {
-    const options = {
+    this.options = {
       from: '"RYM Tracker" <no-reply@rymtracker.ml>',
       to: user.email,
       subject: subject,
       text: `Please use a client with HTML support.`,
       html: html
     };
-    return options;
+    return this.options;
   },
 
   async generate(user, type) {
-    const subject = this.getSubject(type, user);
+    const subject = this.getSubject(user, type);
     const html = await this.getHtml(user, type);
+
     const options = this.getOptions(user, subject, html);
     return options;
   }
