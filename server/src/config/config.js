b4c3c9760d2a12fefe37e7891734bf768e78e87f
dokugo <7773972+dokugo@users.noreply.github.com>
2020-01-31 23:50:07 +0300

    update environment variables management

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,5 +1,14 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: `${__dirname}/.env` });
+
+const development = { path: `${__dirname}/.env.development` };
+const production = { path: `${__dirname}/.env.production` };
+
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config(development);
+}
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config(production);
+}
 
 module.exports = {
   // used in app.js
@@ -7,11 +16,9 @@ module.exports = {
   DB_URI: process.env.MONGODB_URI,
   DB_OPTIONS: { useNewUrlParser: true, useUnifiedTopology: true },
 
-  // used in helpers/utils
+  // used in middlewares/middlewares
   // used for client operations auth
-  API_KEY_CLIENT: process.env.API_KEY_CLIENT,
-  // used for email-accessed routes auth
-  API_KEY_EMAIL: process.env.API_KEY_EMAIL,
+  API_KEY_PUBLIC: process.env.API_KEY_PUBLIC,
   // used for inner server services operations auth (mass crawling, mailing)
   API_KEY_PRIVATE: process.env.API_KEY_PRIVATE,
 
@@ -20,7 +27,7 @@ module.exports = {
   RATE_LIMIT_MAX: 10, // limit each IP to RATE_LIMIT_MAX requests per windowMs
 
   // used in services/mailer/mailer
-  SMTP_HOST: process.env.SMTP_HOST_DEV || process.env.SMTP_HOST_PROD,
-  SMTP_USER: process.env.SMTP_USER_DEV || process.env.SMTP_USER_PROD,
-  SMTP_PASS: process.env.SMTP_PASS_DEV || process.env.SMTP_PASS_PROD
+  SMTP_HOST: process.env.SMTP_HOST,
+  SMTP_USER: process.env.SMTP_USER,
+  SMTP_PASS: process.env.SMTP_PASS
 };
